8632445544ab8e232bc2815e04cf4c5a53b95d43
hussain2023 <iamhussainali2@gmail.com>
2022-10-07 16:53:13 +0530

    added some style s of cart in order section

--- src/components/ReviewItem/ReviewItem.js
+++ src/components/ReviewItem/ReviewItem.js
@@ -1,8 +1,10 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 import "./ReviewItem.css";
 
 const ReviewItem = ({ product }) => {
-  const { name, price, quantity, img } = product;
+  const { name, price, quantity, img, shipping } = product;
   return (
     <div className="review-item">
       <div>
@@ -12,14 +14,22 @@ const ReviewItem = ({ product }) => {
         <div className="review-details">
           <p>{name}</p>
           <p>
-            <small>Price: {price}</small>
+            <small>Price: ${price}</small>
           </p>
           <p>
-            <small>Quantity: {quantity}</small>
+            <small>Shipping: ${shipping}</small>
+          </p>
+          <p>
+            <small>Quantity: ${quantity}</small>
           </p>
         </div>
         <div className="delete-container">
-          <button>Delete</button>
+          <button className="btn-delete">
+            <FontAwesomeIcon
+              className="delete-icon"
+              icon={faTrashAlt}
+            ></FontAwesomeIcon>
+          </button>
         </div>
       </div>
     </div>
