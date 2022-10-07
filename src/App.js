69a5c7b9c0f77d47f1ac9c3b7fb9301474f0441b
hussain2023 <iamhussainali2@gmail.com>
2022-10-07 10:14:56 +0530

    add nested routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
+import About from "./components/About/About";
+import Inventory from "./components/Inventory/Inventory";
+import Orders from "./components/Orders/Orders";
 import Main from "./layouts/Main";
+import Shop from "./components/Shop/Shop";
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <Main></Main>,
+      children: [
+        {
+          path: "/",
+          element: <Shop></Shop>,
+        },
+        {
+          path: "/shop",
+          element: <Shop></Shop>,
+        },
+        {
+          path: "/orders",
+          element: <Orders></Orders>,
+        },
+        {
+          path: "/inventory",
+          element: <Inventory></Inventory>,
+        },
+        {
+          path: "/about",
+          element: <About></About>,
+        },
+      ],
     },
   ]);
   return (
